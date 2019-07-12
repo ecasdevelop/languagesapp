cde61025e3cadf634cae2f79cee9d8618ab6471a
Erick Castillo <cler@praxis.com.mx>
2019-07-12 17:21:26 -0500

    .

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ app.route('/api/cats').get((req, res) => {
 //     console.log('Server started!')
 // })
 
-// app.listen(process.env.PORT || 3000, function() {
-//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-// });
-
-app.listen(process.port, function() {
-    console.log('Express server listening on %d, in %s mode', this.address().port, app.get('env'));
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+// app.listen(process.port, function() {
+//     console.log('Express server listening on %d, in %s mode', this.address().port, app.get('env'));
+// });
