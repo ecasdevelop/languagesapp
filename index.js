a0fcbdc798fd06cb59f27289ddb5575daf198484
erick <ecasdevelop@gmail.com>
2019-07-11 23:04:20 -0500

    /

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 let msg = require("./server")
 
+app.route('/').get((req, res) => {
+    console.log(msg.servidor());
+    res.send("Hola mundo")
+})
+
 app.route('/api/cats').get((req, res) => {
     console.log(msg.servidor());
     res.send({
         cats: [{ name: 'lilly' }, { name: 'lucy' }],
     })
 })
+
 
 //starting server
 app.listen(8000, () => {
