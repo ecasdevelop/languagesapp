0d377cde15c4426be833a984d1896ff4a4aa0778
erick <ecasdevelop@gmail.com>
2019-07-11 23:35:42 -0500

    arreglo

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ app.route('/api/cats').get((req, res) => {
 // app.listen(8000, () => {
 //     console.log('Server started!')
 // })
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+
+// app.listen(process.env.PORT || 3000, function() {
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
+
+app.listen(process.port, function() {
+    console.log('Express server listening on %d, in %s mode', this.address().port, app.get('env'));
 });
